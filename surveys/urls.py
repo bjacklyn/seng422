1624f55d505e769133058e7cb999e653612b2c17
luhaolu <therealhaolu@gmail.com>
2014-07-11 14:28:25 -0700

    added survey list page, and created dummy views for create, delete and display

--- surveys/urls.py
+++ surveys/urls.py
@@ -4,4 +4,7 @@ from surveys import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+	url(r'/create$', views.create, name='create'),
+	url(r'/delete$', views.delete, name='delete'),
+	url(r'/display$', views.display, name='display'),
 )
--- surveys/views.py
+++ surveys/views.py
@@ -1,11 +1,40 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_protect
 
 # Create your views here.
 @login_required
 def index(request):
+	
+	template = loader.get_template('surveys/surveys_list_page.html')
+	
+	context = RequestContext(request, {
+            'user': request.user,
+			'group': request.user.groups.all()[0].name,
+			
+			#using the user list for now
+			'surveys': User.objects.all()
+			
+            })
+	
 	if request.user.groups.all()[0].name == "Manager":
-		return HttpResponse('Welcome Manager %s!' % request.user.username)
+		pass
+		#return HttpResponse('Welcome Manager %s!' % request.user.username)
 	else:
-		return HttpResponse('Welcome Surveyor %s!' % request.user.username)
+		pass
+		#return HttpResponse('Welcome Surveyor %s!' % request.user.username)
+
+	return HttpResponse(template.render(context))
+
+def create(request):
+	return HttpResponse('Create your survey HERE!!! %s!' % request.user.username)
+
+@csrf_protect
+def delete(request):
+	checkboxes = request.POST.getlist('checked_surveys')
+	return HttpResponse('Delete your survey HERE!!! %s!' % checkboxes)
+	
+def display(request):
+	return HttpResponse('Your survey is displayed HERE!!! %s!' % request.user.username)
